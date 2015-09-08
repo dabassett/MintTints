c5fa2cfc1a2d6c81dd769aa90fcb0eaa6712eac4
David Bassett <dabassett.github@gmail.com>
2015-09-08 17:50:06 -0400

    Add initial project code
    Now displays random color boxes. Clicking the colors brings up a
    modal with more information and text contrast samples.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,121 @@
 'use strict';
 
-var jQuery = require('jquery');
-var $ = jQuery;
-var tinycolor = require('tinycolor');
-var bootstrap = require('bootstrap'); // todo pick only what you need
+global.jQuery = require('jquery');
+var $ = global.jQuery
+var tc = require('tinycolor');
+require('bootstrap-sass'); // todo pick only what you need
 
-jQuery(function() {
-  console.log(bootstrap);
-  console.log($);
-  console.log(tinycolor);
+$(function() {
+  var $colorModal = $('#color-info');
+  var $infoBox = {
+    modal:    $colorModal,
+    header:   $colorModal.find('.modal-header'),
+    body:     $colorModal.find('.modal-body'),
+    title:    $colorModal.find('.modal-title'),
+    lum:      $colorModal.find('#luminance'),
+    rgb:      $colorModal.find('#rgb'),
+    contrast: $colorModal.find('#max-contrast'),
+    stats:    $colorModal.find('.stats'),
+    samples:  $colorModal.find('.text-samples'),
+    aaLarge:  $colorModal.find('#aa-large'),
+    aaaLarge: $colorModal.find('#aaa-large'),
+    aaaSmall: $colorModal.find('#aaa-small')
+  };
+
+  function maxContrast(color) {
+    return Math.round(
+      Math.max(tc.readability(color, 'black'),
+               tc.readability(color, 'white')) * 10) / 10;
+  }
+
+  $.fn.addColorLabel = function(color, contrastColor) {
+    $('<h4 class="contrast">' + color.toHexString() + '</h4>')
+      .appendTo(this)
+      .css({
+        'color': contrastColor
+      });
+    return this;
+  };
+
+  function addColorBlock(color) {
+    var hswl = color.toHswl();
+    var colors = {};
+    colors.orig = color;
+    colors.aaLarge = tc.getReadable(color, {level: 'aa', size: 'large', returnBestFit: true});
+    colors.aaaLarge = tc.getReadable(color, {level: 'aaa', size: 'large', returnBestFit: true});
+    colors.aaaSmall = tc.getReadable(color, {level: 'aaa', size: 'small', returnBestFit: true});
+    colors.h = hswl.h;
+    colors.s = hswl.s;
+    colors.lum = hswl.wl;
+    colors.dark = tc({h: colors.h, s: colors.s * 0.50, wl: colors.lum * 0.25});
+    colors.darkText = tc.getReadable(colors.dark, {contrastRatio: 6, returnBestFit: true});
+    colors.divider = '2px solid ' + colors.aaaLarge.toHexString();
+
+    var $column = $('<div class="col-lg-2 col-md-3 col-sm-4 col-xs-6"></div>').appendTo($('.colors'));
+
+    $('<div class="tint"></div>')
+      .appendTo($column)
+      .addColorLabel(colors.orig, colors.aaaLarge)
+      .css({
+        'background': colors.orig.toHexString()
+      })
+      .hover(function() {
+        $(this).css({
+          'border-color': colors.aaaLarge.toHexString(),
+          'box-shadow': '0 0 5px ' + colors.aaaLarge.toHexString(),
+        })
+      }, function() {
+        $(this).css({
+          'border-color': 'transparent',
+          'box-shadow': ''
+        })
+      })
+      .click(function() {
+        $infoBox.header.css({
+          'background-color': colors.orig.toHexString(),
+          'border-bottom': colors.divider
+        });
+        $infoBox.title.css('color', colors.aaaLarge.toHexString());
+        $infoBox.title.text('Information for ' + colors.orig.toHexString());
+
+        $infoBox.stats.css('background-color', colors.dark.toHexString());
+        $infoBox.stats.find('p').css('color', colors.darkText.toHexString());
+        $infoBox.rgb.text(colors.orig.toRgbString());
+        $infoBox.lum.text(colors.lum);
+        $infoBox.contrast.text(maxContrast(colors.orig));
+
+        $infoBox.samples.css({
+          'background-color': colors.orig.toHexString(),
+          'border-top': colors.divider
+        })
+          .find('h3')
+          .css('color', colors.aaaLarge.toHexString());
+        $infoBox.aaLarge.css('color', colors.aaLarge.toHexString())
+          .find('.hex')
+          .text(colors.aaLarge.toHexString());
+        $infoBox.aaaLarge.css('color', colors.aaaLarge.toHexString())
+          .find('.hex')
+          .text(colors.aaaLarge.toHexString());
+        $infoBox.aaaSmall.css('color', colors.aaaSmall.toHexString())
+          .find('.hex')
+          .text(colors.aaaSmall.toHexString());
+
+        $infoBox.modal.modal();
+      });
+
+  }
+
+  function addColors() {
+    // populate the random colors list
+    for(var i = 0; i < 48; i++) {
+      var randomColor = tc.random();
+      addColorBlock(randomColor);
+    }
+  }
+
+  $('#more-colors').click(function() {
+    addColors();
+  });
+
+  addColors();
 });
